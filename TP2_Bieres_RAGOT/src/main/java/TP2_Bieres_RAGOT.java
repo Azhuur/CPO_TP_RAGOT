3fc4fd877a5c8f76c3beb3012405c8eeaac64788
Azhur <arthur.ragot@icloud.com>
2023-10-11 11:14:56 +0200

    Création d'un nouveau projet versionné

---
+++ TP2_Bieres_RAGOT/src/main/java/TP2_Bieres_RAGOT.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Arthur
+ */
+public class TP2_Bieres_RAGOT {
+BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des trolls",
+7 ,"Dubuisson") ;
+
+
+}
